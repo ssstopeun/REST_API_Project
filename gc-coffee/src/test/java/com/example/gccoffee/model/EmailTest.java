d9d6607b39d300953935a420c29c38e436593d27
정지은 <68415975+ssstopeun@users.noreply.github.com>
2022-08-31 16:11:33 +0900

    Initial commit

---
+++ gc-coffee/src/test/java/com/example/gccoffee/model/EmailTest.java
@@ -0,0 +1,20 @@
+package com.example.gccoffee.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+
+    @Test
+    public void testInvalidEmail(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            var email= new Email("accccc");
+        });
+    }
+    @Test
+    public void testValidEmail(){
+        var email= new Email("hello@gmail.com");
+        assertTrue(email.getAddress().equals("hello@gmail.com"));
+    }
+}
