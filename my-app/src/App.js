1fe78e6436c306bbe04e74d0d7c5b922ce4f98fe
정지은 <68415975+ssstopeun@users.noreply.github.com>
2022-10-21 15:45:23 +0900

    make category

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,6 +12,14 @@ function App() {
         {productId: 'uuid-2', productName: '콜롬비아 커피 2', category: '커피빈', price: 5000},
         {productId: 'uuid-3', productName: '콜롬비아 커피 3', category: '커피빈', price: 5000},
     ]);
+
+    const [fixProducts, setFixProducts] = useState([
+        axios.get('http://localhost:7456/api/v1/products')
+            .then(v => setFixProducts(v.data))
+        ]
+    );
+
+
     const [items, setItems] = useState([]);
     const handleAddClicked = productId => {
         const product = products.find(v => v.productId === productId);
@@ -24,10 +32,12 @@ function App() {
         setItems(updatedItems);
     }
 
+
+
     useEffect(() => {
         axios.get('http://localhost:7456/api/v1/products')
             .then(v => setProducts(v.data));
-    }, []);
+    }, [])
 
     const handleOrderSubmit = (order) => {
         if (items.length === 0) {
@@ -58,6 +68,31 @@ function App() {
                     console.error(e);
                 })
         }
+
+    }
+
+    const handleAllClicked = () => {
+        setProducts(fixProducts);
+    }
+
+    const handleBeanClicked = () => {
+        const beanProducts = fixProducts.filter(v => v.category === 'COFFEE_BEAN_PACKAGE');
+        setProducts(beanProducts);
+    }
+
+    const handleTeaClicked = () => {
+        const beanProducts = fixProducts.filter(v => v.category === 'TEA');
+        setProducts(beanProducts);
+    }
+
+    const handleMDClicked = () => {
+        const beanProducts = fixProducts.filter(v => v.category === 'MD');
+        setProducts(beanProducts);
+    }
+
+    const handleDripBagClicked = () => {
+        const beanProducts = fixProducts.filter(v => v.category === 'DRIP_BAG');
+        setProducts(beanProducts);
     }
 
     return (
@@ -69,6 +104,16 @@ function App() {
             <div className="card">
                 <div className="row">
                     <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+                        <h5 className="mx-2">Category</h5>
+                        <div>
+                            <button onClick={handleAllClicked} className = "mx-2">All</button>
+                            <button onClick={handleBeanClicked} className = "mx-2">Coffee Bean</button>
+                            <button onClick={handleDripBagClicked} className = "mx-2">Drip Bag</button>
+                            <button onClick={handleTeaClicked} className = "mx-2">Tea</button>
+                            <button onClick={handleMDClicked} className = "mx-2">MD</button>
+                        </div>
+
+                        <hr />
                         <ProductList products={products} onAddClick={handleAddClicked}/>
                     </div>
                     <div className="col-md-4 summary p-4">
--- my-app/src/components/ProductList.js
+++ my-app/src/components/ProductList.js
@@ -2,12 +2,12 @@ import React from "react";
 import {Product} from "./Product";
 
 export function ProductList({products = [], onAddClick}) {
+
+
     return (
         <React.Fragment>
             <h4 className="mx-2"><b>상품 목록</b></h4>
-            <h8 className="mx-2">
-                <button>coffee been package</button>
-            </h8>
+
             <ul className="list-group products">
                 {products.map(v =>
                     <li key={v.productId} className="list-group-item d-flex mt-3">
